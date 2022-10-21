f2fb5094b2f55c1a4623f24b54691049d702e2c8
simDev1234 <simDev1234@gmail.com>
2022-10-22 00:51:51 +0900

    build: start project

---
+++ src/main/java/com/example/petbutler/PetbutlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.petbutler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetbutlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetbutlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/petbutler/PetbutlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petbutler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetbutlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
