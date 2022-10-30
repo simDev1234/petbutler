10294563ab1b5f85be1ef50f0e4c0321dd68de72
simDev1234 <simDev1234@gmail.com>
2022-10-31 00:26:20 +0900

    file: html, js, css
    Background
    ---
    기존 프론트단이 너무 복잡
    
    Change
    ---
    기능에 포커스를 맞추어 수정

--- src/main/resources/static/js/sign-up-seller.js
+++ src/main/resources/static/js/sign-up-seller.js
@@ -1,9 +1,6 @@
 /* 비밀번호 규칙 */
 const reg = /(?=.*\d{1,50})(?=.*[~`!@#$%\^&*()-+=]{1,50})(?=.*[a-zA-Z]{2,50}).{8,50}/;
 
-/* 집사 경력 숫자 여부 */
-const reg_num = /[0-9]{1,2}/;
-
 /* 비밀번호 유효성 체크 */
 function passwordCheck(){
   let password = $("#password").val();
@@ -39,21 +36,6 @@ function passwordCheck(){
   return true;
 }
 
-/* 집사 경력 유효성 체크 */
-function butlerLevelCheck(){
-  let butlerLevel =  $("#butlerLevel").val();
-
-  if (butlerLevel == null) {
-    alert('집사 경력을 작성해주세요.');
-    return false;
-  }
-
-  if (!reg_num.test(butlerLevel)) {
-    alert('집사 경력은 숫자 일의자리수 또는 십의자리수로 작성해주세요.');
-    return false;
-  }
-}
-
 /* 회원가입 */
 function register(f){
 
@@ -61,27 +43,32 @@ function register(f){
     return false;
   }
 
-  if (butlerLevelCheck() == false) {
-    return false;
-  }
-
   f.submit();
   return true;
 }
 
-/* 집사 경력 셀렉박스 */
-$(function(){
-  $("select").on('change',function(){
+/* 우편 주소 */
+function execDaumPostcode() {
+  new daum.Postcode({
+    oncomplete: function(data) {
+      // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
 
-    $("#butlerLevel").attr('placeholder',this.value);
+      // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+      // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+      var addr = ''; // 주소 변수
 
-    if (this.value == '직접입력') {
-      $("#butlerLevel").focus();
-    } else {
-      $("#butlerLevel").attr('readonly',false);
-      console.log(this.value);
-      $("#butlerLevel").val(this.value);
+      //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+      if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+        addr = data.roadAddress;
+      } else { // 사용자가 지번 주소를 선택했을 경우(J)
+        addr = data.jibunAddress;
+      }
+
+      // 우편번호와 주소 정보를 해당 필드에 넣는다.
+      document.getElementById('postcode').value = data.zonecode;
+      document.getElementById("address").value = addr;
+      // 커서를 상세주소 필드로 이동한다.
+      document.getElementById("detailAddress").focus();
     }
-
-  });
-});
+  }).open();
+}
