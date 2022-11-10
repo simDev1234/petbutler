376a48934369c67e5c74378d282642f0d3183af3
simDev1234 <simDev1234@gmail.com>
2022-11-10 17:14:44 +0900

    feat : 예외 처리 관련 로직 수정
    - 커스텀 예외 및 기타 예외 발생 시, error-msg.html 뷰로 이동
    - 로그인 예외 발생 시, access-denied.html 뷰로 이동

--- src/main/java/com/example/petbutler/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/petbutler/exception/GlobalExceptionHandler.java
@@ -1,14 +1,12 @@
 package com.example.petbutler.exception;
 
-import static com.example.petbutler.exception.type.ErrorCode.INTERNAL_SERVER_ERROR;
-import static com.example.petbutler.exception.type.ErrorCode.INVALID_REQUEST;
-
 import com.example.petbutler.exception.type.ErrorCode;
-import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailSendException;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,48 +18,70 @@ public class GlobalExceptionHandler {
 
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   @ExceptionHandler(ButlerUserException.class)
-  public ErrorResponse handleButlerException(ButlerUserException e) {
+  public String handleButlerUserException(ButlerUserException e, Model model) {
 
     ErrorCode errorCode = e.getErrorCode();
 
     log.error("{} is occured.", errorCode.name());
 
-    return new ErrorResponse(errorCode.name(), errorCode.getMessage());
+    model.addAttribute("errorResponse",
+        new ErrorResponse(errorCode.name(), errorCode.getMessage()));
+
+    return "exception/error-msg";
+  }
+
+  @ExceptionHandler(ButlerCategoryException.class)
+  public ResponseEntity handleButlerCategoryException(ButlerCategoryException e) {
+
+    ErrorCode errorCode = e.getErrorCode();
+
+    log.error("{} is occured.", errorCode.name());
+
+    return new ResponseEntity(new ErrorResponse(errorCode.name(), errorCode.getMessage()), HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(DataIntegrityViolationException.class)
-  public ErrorResponse handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+  public String handleDataIntegrityViolationException(DataIntegrityViolationException e, Model model) {
+
     log.error("DataIntegrityViolationException is occured.", e);
 
-    return new ErrorResponse(INVALID_REQUEST.name(), INVALID_REQUEST.getMessage());
-  }
+    model.addAttribute("errorResponse",
+        new ErrorResponse("DataIntegrityViolationException", e.getMessage()));
 
-  @ExceptionHandler(IOException.class)
-  public ErrorResponse handleIOException(IOException e) {
-    log.error("IOException is occured.", e);
-
-    return new ErrorResponse(ErrorCode.FILE_CONTENTS_NOT_EXIST.name(), INVALID_REQUEST.getMessage());
+    return "exception/error-msg";
   }
 
   @ExceptionHandler(MailSendException.class)
-  public ErrorResponse handleMailSendException(MailSendException e){
+  public String handleMailSendException(MailSendException e, Model model){
+
     log.error("MailSendException is occured.", e);
 
-    return new ErrorResponse(INVALID_REQUEST.name(), INVALID_REQUEST.getMessage());
+    model.addAttribute("errorResponse",
+        new ErrorResponse("MailSendException", e.getMessage()));
+
+    return "exception/error-msg";
   }
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+  public String handleMethodArgumentNotValidException(MethodArgumentNotValidException e, Model model) {
+
     log.error("MethodArgumentNotValidException is occured.", e);
 
-    return new ErrorResponse(INVALID_REQUEST.name(), INVALID_REQUEST.getMessage());
+    model.addAttribute("errorResponse",
+        new ErrorResponse("MethodArgumentNotValidException", e.getMessage()));
+
+    return "exception/error-msg";
   }
 
   @ExceptionHandler(Exception.class)
-  public ErrorResponse handleException(Exception e) {
+  public String handleException(Exception e, Model model) {
+
     log.error("Exception is occured.", e);
 
-    return new ErrorResponse(INTERNAL_SERVER_ERROR.name(), INTERNAL_SERVER_ERROR.getMessage());
+    model.addAttribute("errorResponse",
+        new ErrorResponse("Exception", e.getMessage()));
+
+    return "exception/error-msg";
   }
 
 }
